7879d97856a58bb6bd5c763936eab53801c148ce
daskilet <daskilet@mail.ru>
2013-04-23 20:19:01 +0300

    deply_improvement9

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 from setuptools import setup
 
-<<<<<<< HEAD
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-=======
 setup(name='Andrey',
       version='1.0',
       description='OpenShift App',
@@ -16,5 +7,5 @@ setup(name='Andrey',
       author_email='daskilet@mail.ru',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Django>=1.5','MySQL-python'],
->>>>>>> master
+
      )
