3b88b67d0668feafa7cb0e8d365a0c654351b100
daskilet <daskilet@mail.ru>
2013-04-24 17:59:27 +0300

    lets see

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='Andrey',
       author='daskilet',
       author_email='daskilet@mail.ru',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Django<=1.4','MySQL-python','haystack','PIL','south'],
+      install_requires=['Django<=1.4','MySQL-python','django-haystack','PIL','south'],
      )
      
