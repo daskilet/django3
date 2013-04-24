f329e116e8edb79da5b66d892dd5339d2199987e
daskilet <daskilet@mail.ru>
2013-04-24 18:39:51 +0300

    lets see

--- wsgi/myblog/settings.py
+++ wsgi/myblog/settings.py
@@ -115,9 +115,9 @@ TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
 ROOT_URLCONF = 'myblog.urls'
 
 TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__),'templates').replace('\\','/'),
+    os.path.join(PROJECT_DIR,'templates'),
 )
-   
+print TEMPLATE_DIRS[0]   
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
