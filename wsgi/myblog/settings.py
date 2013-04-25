44394ec786411b3e1027299109adf3ccec5b4916
daskilet <daskilet@mail.ru>
2013-04-25 10:04:21 +0300

    lets see

--- wsgi/myblog/settings.py
+++ wsgi/myblog/settings.py
@@ -113,8 +113,7 @@ TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
 "django.core.context_processors.media",
 "django.core.context_processors.static",
 "django.core.context_processors.tz",
-"django.contrib.messages.context_processors.messages",
-"gitrevision.context_processors.gitrevision",)
+"django.contrib.messages.context_processors.messages",)
 
 ROOT_URLCONF = 'myblog.urls'
 
