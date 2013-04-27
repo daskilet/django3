6e9929503a72a917bba6f1989cbe90493cd230d9
daskilet <daskilet@mail.ru>
2013-04-27 20:06:30 +0300

    imprivements continue

--- wsgi/myblog/myblog/settings.py
+++ wsgi/myblog/myblog/settings.py
@@ -141,20 +141,3 @@ INSTALLED_APPS = (
     'south',
     'myblog.blog',
 )
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
