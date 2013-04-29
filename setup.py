a206b31fbb6743fe6dfe60bd3cb394c0b45519a7
daskilet <daskilet@mail.ru>
2013-04-29 23:01:11 +0300

    imprivements continue

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='Andrey',
       author_email='daskilet@mail.ru',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['Django<=1.5','MySQL-python','django-haystack==1.2.6','PIL','south',
-      'simplejson','whoosh==2.4.1','disqus','django-disqus'],
+      'simplejson','whoosh==2.4.1','django-disqus'],
      )
      
