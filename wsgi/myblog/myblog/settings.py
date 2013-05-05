56938cf9b3e5a756ce10e000ebb196ccc0a06ff3
daskilet <daskilet@mail.ru>
2013-05-05 17:09:32 +0300

    imprivements continue

--- wsgi/myblog/myblog/settings.py
+++ wsgi/myblog/myblog/settings.py
@@ -111,7 +111,8 @@ TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
 "django.core.context_processors.media",
 "django.core.context_processors.static",
 "django.core.context_processors.tz",
-"django.contrib.messages.context_processors.messages",)
+"django.contrib.messages.context_processors.messages",
+'gitrevision.context_processors.gitrevision',)
 GOOGLE_ANALYTICS_MODEL = True
 ROOT_URLCONF = 'myblog.urls'
 
@@ -135,6 +136,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
      'django.contrib.admin',
      'analytical',
+     'gitrevision',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'disqus',
