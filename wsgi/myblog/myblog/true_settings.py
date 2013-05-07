bef7459dc3ed196eed3a0fb6f7951a44ee0e4255
daskilet <daskilet@mail.ru>
2013-05-07 20:08:59 +0300

    imprivements continue

---
+++ wsgi/myblog/myblog/true_settings.py
@@ -0,0 +1,6 @@
+# -*- coding: utf8 -*-
+# Django settings for myblog project.
+database_password = 'daskilet463804'
+GOOGLE_ID = 'UA-40628880-1'
+
+DISQUS_API = 'tBcLV80hzwZ42GbCSXnE5aOne8Mu9XxMavhVLmgETQilNgvEJzjujDUPcgVAXgLs'
