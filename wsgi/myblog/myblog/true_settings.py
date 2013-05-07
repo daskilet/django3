105904242004f66ee6dc389194ed868ee6130c0e
daskilet <daskilet@mail.ru>
2013-05-07 20:21:20 +0300

    imprivements continue

--- wsgi/myblog/myblog/true_settings.py
+++ wsgi/myblog/myblog/true_settings.py
@@ -4,3 +4,4 @@ database_password = 'daskilet463804'
 GOOGLE_ID = 'UA-40628880-1'
 
 DISQUS_API = 'tBcLV80hzwZ42GbCSXnE5aOne8Mu9XxMavhVLmgETQilNgvEJzjujDUPcgVAXgLs'
+GOOGLE_PASSWORD = 'daskilet46380442'
