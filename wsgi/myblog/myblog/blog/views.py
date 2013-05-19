788bfad8d94cdf08cd0061620f462d4d1df9b0db
daskilet <daskilet@mail.ru>
2013-05-19 18:28:13 +0300

    imprivements continue

--- wsgi/myblog/myblog/blog/views.py
+++ wsgi/myblog/myblog/blog/views.py
@@ -158,7 +158,7 @@ def search(request):
 	posts = []
 	for r in results:
             posts.append(r.object)
-        #posts = [element for element in posts if element]
+        posts = [element for element in posts if element]
 	#videos list contains all the videos those match the search criteria
 	return render_to_response('blog/poisk_rezult.html',dict({'spisok_categ':categories_spisok(),'spisok_publ':archive(),'text':posts,'site': ssil.ssilka},\
                                   **recent_polls(request)),context_instance=ssil.context)
