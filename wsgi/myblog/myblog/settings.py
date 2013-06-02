2fcd70ed53c0a1a0c58e87abac5008dcacc8c88e
daskilet <daskilet@mail.ru>
2013-06-02 13:42:32 +0300

    Yet another some unimportant improvements

--- wsgi/myblog/myblog/settings.py
+++ wsgi/myblog/myblog/settings.py
@@ -98,7 +98,7 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
 )
-put_k_custom_middleware = 'myblog.customm.middleware.debug'
+put_k_custom_middleware = 'myblog.customm.debug'
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
      put_k_custom_middleware+'.UserBasedExceptionMiddleware',
