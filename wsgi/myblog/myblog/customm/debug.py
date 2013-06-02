c86836888ce5d9cbe73a27b75518c02f51174d0a
daskilet <daskilet@mail.ru>
2013-06-02 13:52:55 +0300

    Yet another some unimportant improvements

--- wsgi/myblog/myblog/customm/debug.py
+++ wsgi/myblog/myblog/customm/debug.py
@@ -4,4 +4,5 @@ from django.conf import settings
 class UserBasedExceptionMiddleware(object):
     def process_exception(self, request, exception):
         if request.user.is_superuser or request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS:
+	    print 'hello'
             return technical_500_response(request, *sys.exc_info())
