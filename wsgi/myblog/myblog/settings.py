ce3d4257599d617123fd624005edb3df678d78ec
daskilet <daskilet@mail.ru>
2013-06-02 13:08:55 +0300

    Yet another some unimportant improvements

--- wsgi/myblog/myblog/settings.py
+++ wsgi/myblog/myblog/settings.py
@@ -101,7 +101,7 @@ TEMPLATE_LOADERS = (
 if not ON_OPENSHIFT:
   put_k_custom_middleware = 'myblog.custom'
 else:
-     put_k_custom_middleware=os.path.join(os.environ.get('OPENSHIFT_REPO_DIR'), 'wsgi', 'myblog','myblog','custom')
+     put_k_custom_middleware=os.environ.get('OPENSHIFT_REPO_DIR')+'.wsgi.myblog.myblog.custom'
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
      put_k_custom_middleware+'.UserBasedExceptionMiddleware',
